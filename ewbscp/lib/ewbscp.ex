9bc7103291b83968a2ce31de72b3706bc1f49f6a
Johnny Carvalho <johnny.carvalho@contaswap.com>
2023-12-01 11:47:43 -0300

    first fucking commit

---
+++ ewbscp/lib/ewbscp.ex
@@ -0,0 +1,18 @@
+defmodule Ewbscp do
+  @moduledoc """
+  Documentation for `Ewbscp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ewbscp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
