12f889ecfa0c125a9c5f23e771764530334d7af6
Matheus Barão Fiorin <matheus.fiorin@nubank.com.br>
2023-12-15 09:19:43 -0300

    Update external cli adapter to use the workflow

--- lib/adapter/external/cli.ex
+++ lib/adapter/external/cli.ex
@@ -1,54 +1,14 @@
 defmodule CLI do
   def main(args) do
-    parsed =
-      args
-      |> OptionParser.parse(strict: [allow_empty: :boolean])
+    parsed = args |> OptionParser.parse(strict: [allow_empty: :boolean])
 
-    with {options, [directory], []} <- parsed,
-         true <- File.dir?(directory),
-         {:ok, files} <- list_files(directory) do
-      Enum.reduce(files, 0, &(&2 + calculate_file_size(&1)))
-      |> print_size(files, options)
+    with {options, [command | command_args], []} <- parsed do
+      cli_command = %CLICommand{command: command, args: {command_args, options}}
+      CLIHandler.handle_command(cli_command)
     else
       _ ->
         IO.puts("Invalid input")
         System.halt(1)
     end
-  end
-
-  defp print_size(0, _, options) do
-    case Keyword.get(options, :allow_empty) do
-      true ->
-        IO.puts("0")
-
-      _ ->
-        IO.puts("Empty directory")
-        System.halt(1)
-    end
-  end
-
-  defp print_size(total_size, files, _) do
-    IO.puts(div(total_size, length(files)))
-  end
-
-  defp list_files(directory) do
-    case File.ls(directory) do
-      {:ok, files} ->
-	    files =
-          files
-          |> Enum.map(&Path.join(directory, &1))
-          |> Enum.reject(&File.dir?/1)
-
-		{:ok, files}
-
-      error ->
-        error
-    end
-  end
-
-  defp calculate_file_size(file) do
-    file
-    |> File.stat!()
-    |> Map.get(:size)
   end
 end
