d441e6e19ffb53ed95d915f48f12f46fe3139430
Matheus Barão Fiorin <matheus.fiorin@nubank.com.br>
2023-12-15 12:00:54 -0300

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -22,6 +22,7 @@ defmodule Ewbscp.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:tesla, "~> 1.8"},
       {:httpoison, "~> 2.0"},
       {:floki, "~> 0.35.0"}
     ]
