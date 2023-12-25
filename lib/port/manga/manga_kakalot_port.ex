3cd3b4816d01592b56b970d9f81b9b883f6a7820
Johnny Carvalho <johnny.carvalho@contaswap.com>
2023-12-25 15:20:38 -0300

    Change return_search_results function to return a Manga struct

--- lib/port/manga/manga_kakalot_port.ex
+++ lib/port/manga/manga_kakalot_port.ex
@@ -1,10 +1,21 @@
 defmodule Ewbscp.Ports.Manga.MangaKakalot do
+
+  @moduledoc """
+  This module serves as the layer to transform the data into something that makes sense to our Application Core.
+  These functions specifically will handle the web scrapping of the pages from the specific source - MangaKakalot website - and transforms them
+  into Manga Structs and Chapter structs, to be used later in the workflow of the application
+  """
+
+  alias Ewbscp.Application.Domain.Manga.Manga
+
   def return_search_results(manga_search_results) do
-    manga_search_results
-    |> Floki.find(".panel_story_list > .story_item > .story_item_right > h3 > a")
-    |> Enum.map(fn {_, [_, {_, manga_uri}], [manga_title]} ->
-     %{manga_uri: manga_uri, manga_title: manga_title} 
-    end)
+    with {:ok, parsed_document} <- Floki.parse_document(manga_search_results) do
+      parsed_document
+      |> Floki.find(".panel_story_list > .story_item > .story_item_right > h3 > a")
+      |> Enum.map(fn {_, [_, {_, manga_uri}], [manga_title]} ->
+        %Manga{manga_uri: manga_uri, manga_title: manga_title}
+      end)
+    end
   end
 
   def find_chapters_list(manga_title_page) do
